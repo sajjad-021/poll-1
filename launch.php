24e49eb0cc99d1f6ab71eee9dfc8660632f59ec3
yenashenakhte2016 <mmnsfyrad@gmail.com>
2016-08-08 03:30:47 +0430

    Add files via upload

--- launch.php
+++ launch.php
@@ -4,7 +4,7 @@ set_time_limit(0);
 
 require_once 'Elemental.php';
 
-define('BOT_TOKEN', 'Token');
+define('BOT_TOKEN', '252071655:AAEnn9q9ogA-c3NFhRXlSRzTtlBOR2sHUAQ');
 
 $bot = new PollBot(BOT_TOKEN, 'PollBotChat');
 $bot->runLongpoll();
